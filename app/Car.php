c5e5b97c771730bc39ea949c0e864c6364dfa5b7
rahee <ra256@njit.edu>
2018-11-10 10:27:55 -0500

    Commit: Fixed a lot of things such as rewriting the Laravel site with controllers and up to the point where I am at my tests

---
+++ app/Car.php
@@ -0,0 +1,7 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Car extends Model
+{
+    var $Make;
+}
---
+++ database/seeds/CarSeeder.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Database\Seeder;
+class CarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Car::class, 50)->create()->each(function ($u) {
+        });
+    }
+}
